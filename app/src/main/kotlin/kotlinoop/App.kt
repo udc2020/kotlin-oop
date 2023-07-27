0ed524047a53ddc0997145fb5f64b08ec9a3703d
ultrasdzcoder <udc-2020@hotmail.com>
2023-07-27 16:03:37 +0100

    update readme

--- app/src/main/kotlin/kotlinoop/App.kt
+++ app/src/main/kotlin/kotlinoop/App.kt
@@ -1,18 +1,6 @@
 package kotlinoop
 
 
-class Humen (){
-
-    constructor(id:Long):this(){
-        println(id)
-    }
-    constructor(id:String):this(){
-        println(id)
-    }
-
-
-}
-
 
 
 fun main() {
---
+++ app/src/main/kotlin/kotlinoop/Humen.kt
@@ -0,0 +1,13 @@
+package kotlinoop
+
+class Humen (){
+
+    constructor(id:Long):this(){
+        println(id)
+    }
+    constructor(id:String):this(){
+        println(id)
+    }
+
+
+}
